54e78d8469d176f76898981dcc5af994701e46a6
h20dragon <h20dragon@outlook.com>
2018-04-10 02:58:22 -0700

    Init

---
+++ test/e2e/conf/ex5.conf.js
@@ -0,0 +1,32 @@
+'use strict';
+
+exports.config = {
+  directConnect: true,
+
+  onPrepare: function onPrepare() {
+    browser.driver.manage().window().setSize(1680, 1050);
+
+    global.isAngularSite = function (flag) {
+      browser.ignoreSynchronization = !flag;
+    };
+
+    global.dvr = browser.driver;
+  },
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['../../../lib/e2e/exercises/ex5.spec.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 5000
+  }
+};
